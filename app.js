b3a13fb29974dc5b9622ab2df26c41ccec8e5af2
TQuocPhu <tqphu240804@gmail.com>
2025-11-01 00:54:42 +0700

    Hoan thanh BACKEND_1

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ app.use((req, res, next) => {
     return next(new ApiError(404, "Resource not found!"));
 });
 
-
 app.use((err, req, res, next) => {
     // Middleware xử lý lỗi tập trung
     // Trong các đoạn code xử lý ở các route khác, gọi next(error) sẽ chuyển về middleware xử lý lỗi này.
